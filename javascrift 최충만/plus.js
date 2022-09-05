e704a64d8107f626f9d3db2dcb380471534d937d
chung1105 <102003130+chung1105@users.noreply.github.com>
2022-09-05 11:28:09 +0900

    9월5일

---
+++ javascrift 최충만/plus.js
@@ -0,0 +1,23 @@
+function plus(num1,num2){
+    let result;
+    result=num1+num2;
+    return result
+}
+
+function mui(num1,num2){
+    let result;
+    result=num1-num2;
+    return result
+}
+
+function mul(num1,num2){
+    let result;
+    result=num1*num2;
+    return result
+}
+
+function div(num1,num2){
+    let result;
+    result=num1/num2;
+    return result
+}
